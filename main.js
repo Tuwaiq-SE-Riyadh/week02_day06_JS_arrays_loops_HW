469d7d90fcd225d912945780aee1117c71737f39
Amal <amalgassam@hotmail.com>
2021-10-10 16:40:18 +0300

    HW1

--- main.js
+++ main.js
@@ -1,3 +1,56 @@
+// For Loop 
+// Q1 
+function summations(arr){
+    let sum=0;
+
+   for(let i=0;i<arr.length;i++){
+       sum=sum+arr[i];
+   }
+   console.log(sum)
+}
+
+
+// Q2
+function Maximum(arr){
+    let max=0;
+    for(let i=0;i<arr.length;i++){
+        if(arr[i]>max){
+            max=arr[i];
+        }
+    }
+    console.log(max)
+}
+
+
+// Q3
+function reverse1(arr){   
+
+         let m =arr.length-1;
+         for(let i=m;i>=0;i--){
+            console.log(arr[i])
+         }
+
+}
+
+// Q4
+ function stars(){
+     for(let i=1;i<=5;i++){
+        
+         for (let j=1;j<=i;j++){
+        
+                console.log("*")
+            
+         }
+         console.log("\n")
+     }
+
+ }
+
+
+
+
+
+
 const characters = [
     {
         name: 'Luke Skywalker',
@@ -28,3 +81,4 @@ const characters = [
         gender: 'male',
     },
 ];
+
