d976fa152ceb9e05c3db716385f0ba0639ed8b3a
FahadTariqiDev <fahad.tariqi17@gmail.com>
2021-10-10 20:15:42 +0300

    new version

--- main.js
+++ main.js
@@ -28,3 +28,78 @@ const characters = [
         gender: 'male',
     },
 ];
+
+function sum(array) { 
+    let sum = 0; 
+    for (let i = 0; i < arr.length; i++) 
+        sum += arr[i]; 
+
+    return sum; 
+} 
+
+function maxNumber(array){
+let max = array[0];
+array.forEach(element => {
+    if(element> max){
+        max =element
+    }
+});
+console.log(max);
+}
+
+function reverseArray(array){
+array.reverse()
+}
+
+function starsPatterns(number){
+    let starts = "";
+
+    for (let i = 1; i <= number; i++) {
+      for (let j = 0; j < i; j++) {
+        starts += "*";
+      }
+      starts += "\n";
+    }
+    console.log(starts);
+
+}
+
+function prime(number){
+  if (number===1) {
+        console.log("Number is not prime");
+      }
+  else if(number === 2){
+    console.log("Number is prime")
+  }
+
+  
+    for(var i = 2; i < number; i++){
+      if(number % i === 0){
+        console.log("Number is NOT prime");
+        return
+      } else{
+        console.log("Number is prime");
+      }
+    }
+    
+}
+
+// Bonus
+
+function second(array){
+
+   let  result = array.map(({ mass}) => ({mass}));
+    console.log(result);
+    const reducer = (previousValue, currentValue) => previousValue + currentValue;
+    console.log(result.reduce(reducer));
+    
+}
+function first(array){
+let result = array.map(({ name, height }) => ({name, height }));
+console.log(result);
+// names
+ result = array.map(({ name}) => ({name}));
+console.log(result);
+}
+
+
