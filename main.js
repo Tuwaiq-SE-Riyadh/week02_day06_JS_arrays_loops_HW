02131388778b8c59d645cd72d7a70007be2d1e9c
manalalzeer <manalmaz01@gmail.com>
2021-10-10 19:28:11 +0300

    upload files

--- main.js
+++ main.js
@@ -28,3 +28,51 @@ const characters = [
         gender: 'male',
     },
 ];
+
+function SumArray(array) {
+
+    let result = 0;
+    for (let i = 0; i < array.length; i++) {
+        result += array[i];  
+    }
+    console.log(result);
+}
+
+function MaximumArray(array) {
+    let max = 0;
+    for (let i = 0; i < array.length; i++) {
+        if(array[i] > max){
+            max = array[i]
+        }
+    }
+    console.log(max);
+}
+
+function ReverseArray(array) {
+    for (let i = array.length-1; i >=0; i--) {
+        console.log(array[i]);
+        
+    }
+    
+}
+
+function Stars(x) {
+    let Star = "";
+    for (let i = 1; i <= x; i++) {
+        for (let j = 1; j <=i; j++) {
+            Star += "*";
+        }
+        Star += "\n";
+      }
+      console.log(Star);
+    }
+
+function Prime(num) {
+    for(var i = 2; i < num; i++){
+        if(num % i === 0) {
+            return false;
+        }else{
+            return num > 1;
+        }    
+    }
+}
