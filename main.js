3e3ef1baa8cf011475f0170b21b545297702034b
AbdulrahmanY <abdulrahmansaddiq@gmail.com>
2021-10-10 16:54:42 +0300

    Home Work solution

--- main.js
+++ main.js
@@ -28,3 +28,71 @@ const characters = [
         gender: 'male',
     },
 ];
+
+
+
+//Numbers' summations     ////////////////////////////////////////////////////////////////////////////////////
+function sumArr(arr){
+    let result = 0
+    for (let i = 0 ; i<arr.length;i++){
+        result+=arr[i]
+    }
+    console.log(result)
+}
+sumArr([1,2,3])
+
+
+//Maximum number       ////////////////////////////////////////////////////////////////////////////////////
+function maxNum(arr){
+    let max = 0
+    for (let i = 0 ; i<arr.length;i++){
+        if(max<arr[i]){
+            max = arr[i]
+        }
+    }
+    console.log(max)
+}
+maxNum([5,2,3])
+
+
+
+//Reverse Array       ////////////////////////////////////////////////////////////////////////////////////
+function reverseArray(arr){
+    console.log(arr.reverse());
+}
+reverseArray([1,2,3])
+
+
+
+//stars pattern     ////////////////////////////////////////////////////////////////////////////////////
+function starsPattern(num){
+    for(let i = 1 ; i <=num;i++){
+      let x = ""
+        for (let j = 0 ;j<i;j++){
+          x = x+"*"
+            
+        }
+        console.log(x)
+    }
+}
+starsPattern(5)
+
+
+
+//Prime number ////////////////////////////////////////////////////////////////////////////////////
+function primeNumber(num){
+    let isPrime = true
+    for (let i =2 ; i< num;i++){
+      if (num % i == 0){
+        isPrime = false
+        break
+      }
+    }
+    if(isPrime==true){
+      console.log("Prime Number")
+    }
+    else {
+      console.log("Not a Prime Number")
+    }
+  }
+  primeNumber(4)
