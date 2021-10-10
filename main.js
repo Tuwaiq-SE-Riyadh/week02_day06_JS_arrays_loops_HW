c046610ab1c51a6d51e0a62202920c7b017bd46b
S-MW <72529306+S-MW@users.noreply.github.com>
2021-10-11 02:52:34 +0300

    solve all Qs except MAP ,REDUCE ..etc

--- main.js
+++ main.js
@@ -1,3 +1,89 @@
+//--------- For loop ---------
+//--------- For loop ---------
+//--------- For loop ---------
+
+// Numbers' summations
+function numbersSummations(arr)
+{
+    let sum = 0
+    for(let i = 0 ; i<= arr.length-1 ; i++)
+    {
+        sum +=arr[i];
+    }
+    console.log("The sum is :" + sum)
+}
+
+// Maximum number
+function maximumNumber(arr)
+{
+    let max = arr[0]
+    for(let i = 0 ; i<= arr.length-1 ; i++)
+    {
+        if(arr[i]>max)
+        {
+            max = arr[i]
+        }
+    }
+    console.log("The max number is :" + max)
+}
+
+// Reverse Array
+function reverseArray(arr)
+{   let reverseOfArray = ""
+    for(let i = arr.length-1 ; i >= 0  ; i--)
+    {
+        reverseOfArray = reverseOfArray + arr[i] +" "
+    }
+    console.log("The Reverse Array : " + reverseOfArray)
+
+}
+
+//--------- Nested for loop ---------
+//--------- Nested for loop ---------
+//--------- Nested for loop ---------
+
+// stars pattern
+function starsPattern(number)
+{
+    let printStars = ""
+    for(let i = 0 ; i<=number ; i++)
+    {
+        for(let j = 0 ; j<=number ; j++)
+    {
+        if(i>j)
+        {
+            printStars = printStars + "* "
+        }
+    }
+        console.log(printStars)
+        printStars = ""  // reset the stars
+    }
+}
+
+//--------- While loop ---------
+//--------- While loop ---------
+//--------- While loop ---------
+
+function isPrime(number)
+{
+    let flag = true
+    for(let i = 2 ; i<=number ; i++)
+    {
+        if(number % i == 0 && i !== number)
+        {
+            flag = false;
+            break
+        }
+    }
+    console.log(flag)
+}
+
+
+
+//--------- Arrays ---------
+//--------- Arrays ---------
+//--------- Arrays ---------
+
 const characters = [
     {
         name: 'Luke Skywalker',
@@ -28,3 +114,22 @@ const characters = [
         gender: 'male',
     },
 ];
+
+
+// --- MAP ---
+// not yet !
+
+// --- REDUCE ---
+// not yet !
+
+// --- FILTER ---
+// not yet !
+
+// --- SORT ---
+// not yet !
+
+// --- EVERY ---
+// not yet !
+
+// --- SOME ---
+// not yet !
