d2ada5bec4662a898a6df8f883560a4fe5109edd
Tameem Alsulmi <tsalsulmi@gmail.com>
2021-10-10 21:08:46 +0300

    Solve Lab

--- main.js
+++ main.js
@@ -28,3 +28,68 @@ const characters = [
         gender: 'male',
     },
 ];
+
+
+const x = [1,2,3,4,5]
+sumAll(x);
+console.log("------------------------------------------");
+maxArr(x);
+console.log("------------------------------------------");
+rev(x);
+console.log("------------------------------------------");
+stars_pattern(5);
+console.log("------------------------------------------");
+prime(4);
+console.log("------------------------------------------");
+getName();
+
+
+
+
+function sumAll(arr){
+  let  sum = 0;
+for(let i = 0 ; i < arr.length ; i++){
+ sum+=arr[i];
+}
+console.log("Sum = "+sum);
+}
+
+
+function maxArr(arr){
+    console.log(Math.max(...arr));
+  }
+
+
+
+  function rev(arr){
+    console.log(x.reverse());
+}
+
+
+function stars_pattern(num){
+
+    for(let i = 0 ; i <= num ; i++){
+        let s = "";
+        for(let j = 0 ; j < i ; j++){
+            s += "*";
+        }
+        console.log(s);
+}}
+
+function prime(num){
+    let count = 0;
+    for(let i = 1 ; i <= num ; i++){
+        if(num % i == 0){count++;}
+    }
+if(count == 2){console.log("true");}
+else{console.log("false");}
+
+}
+
+
+function getName(){
+    characters.map(function(element){
+        console.log(element.name  + " "+ element.height);
+    });
+    }
+    
