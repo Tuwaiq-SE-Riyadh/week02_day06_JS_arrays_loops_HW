4058a88cb022d6fbd21c63f36e3f7b0f3cd2df9b
shahad96 <shahad96.777@gmail.com>
2021-10-11 09:10:04 +0300

    task done

--- main.js
+++ main.js
@@ -28,3 +28,77 @@ const characters = [
         gender: 'male',
     },
 ];
+
+
+function arraySummations(arr){
+
+ let sum=0
+ for(i=0;i<arr.length;i++){
+
+    sum+= arr[i]
+ }
+ console.log(sum)
+}
+
+
+function maximum(arr){
+
+    let max =0
+
+    for(i=0;i<arr.length;i++){
+
+        if(arr[i] > max){
+            max = arr[i]
+        }
+     }
+
+     console.log("The maximum number is "+max)
+} 
+
+
+
+
+function reverse(arr){
+
+    
+
+    for(i=arr.length-1;i>=0;i--){
+
+        console.log(arr[i])
+     }
+
+    }
+
+
+function stars(num){
+
+   
+    for(let i=1; i <= num ; i++){
+        let stars = ''
+        for(let j=1; j <= i ; j++) {
+    stars = stars + '*'
+        
+        }
+        console.log(stars)
+    }
+    
+}
+
+
+function prim(num){
+
+let i=2
+let j=0
+while(i <= num/2){
+
+    if (num%i === 0){
+        console.log(false)
+        j=1
+        break
+    }
+    i++
+}
+      if (j === 0){
+    console.log(true)
+}
+}
