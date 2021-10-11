9bbf1f397d19e6c21706d6f818b9ea60e3055a77
Tameem Alsulmi <tsalsulmi@gmail.com>
2021-10-11 20:40:10 +0300

    Solve Lab

--- main.js
+++ main.js
@@ -31,17 +31,41 @@ const characters = [
 
 
 const x = [1,2,3,4,5]
+console.log("------------------Numbers' summations------------------------");
 sumAll(x);
-console.log("------------------------------------------");
+console.log("------------------Maximum number------------------------");
 maxArr(x);
-console.log("------------------------------------------");
+console.log("------------------Reverse Array------------------------");
 rev(x);
-console.log("------------------------------------------");
+console.log("------------------stars pattern------------------------");
 stars_pattern(5);
-console.log("------------------------------------------");
+console.log("------------------While loop prim------------------------");
 prime(4);
-console.log("------------------------------------------");
+console.log("------------------MAP 1------------------------");
 getName();
+console.log("------------------MAP 2------------------------");
+getfristname();
+console.log("------------------REDUCE 1------------------------");
+Get_total_mass();
+console.log("------------------REDUCE 2------------------------");
+Get_total_c();
+console.log("------------------FILTER 1------------------------");
+m_100();
+console.log("------------------FILTER 2------------------------");
+female();
+console.log("------------------Sort 1------------------------");
+sort_name();
+console.log("------------------Sort 2------------------------");
+sort_g();
+console.log("------------------EVERY 1------------------------");
+e_eye();
+console.log("------------------EVERY 2------------------------");
+e_g();
+console.log("------------------SOME 1------------------------");
+some_m();
+console.log("------------------SOME 2------------------------");
+some_mass();
+
 
 
 
@@ -93,3 +117,83 @@ function getName(){
     });
     }
     
+// ------------------------------------------------------------------
+        
+        function getfristname(){
+            characters.map(function(element){
+                console.log(element.name.split(" ")[0]);
+            });
+            }
+        
+        function Get_total_mass(){
+            const m = characters.reduce(function (a, el) {
+                return el.mass + a;
+              }, 0);
+                console.log(m);
+        }
+        
+        function Get_total_c(){
+            const m = characters.reduce(function (a, el) {
+                return el.name.length + a;
+              }, 0);
+                console.log(m);
+        
+        }
+        
+        function m_100(){
+            const h = characters.filter(function (el) {
+                return el.mass > 100;
+              });
+              
+              console.log(h);
+        }
+        
+        function female(){
+            const m = characters.filter(function (el) {
+                return el.gender === 'female';
+              });
+              
+              console.log(m);
+        }
+        
+          function e_eye(){
+            const m = characters.every(function(el){
+                return el.eye_color === 'blue';
+              })
+              console.log(m)
+        }
+        
+        function e_g(){
+            const m = characters.every(function(el){
+                return el.gender === 'male'
+              })
+              console.log(m)
+        } 
+        
+        function sort_name(){
+            const m = characters.sort(function(e1 , e2){
+                return e1.name.localeCompare(e2.name);
+              })
+              console.log(m)
+        } 
+        
+        function sort_g(){
+            const m = characters.sort(function(e1 , e2){
+                return e1.gender.localeCompare(e2.gender);
+              })
+              console.log(m)
+        } 
+        
+        function some_m(){
+            const m = characters.some(function(el){
+                return el.gender === "male";
+              })
+              console.log(m)
+        } 
+        
+        function some_mass(){
+            const m = characters.some(function(el){
+                return el.mass < 50;
+              })
+              console.log(m)
+        } 
