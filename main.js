98115dae577607ed6ae2f8b09afc36d18cee0d62
noura <norah@MacBook-Air-norah.local>
2021-10-13 09:38:46 +0300

    my hw

--- main.js
+++ main.js
@@ -28,3 +28,108 @@ const characters = [
         gender: 'male',
     },
 ];
+
+//bounse
+//map
+// Get array of objects with just name and height properties
+characters.map(function(element, index){
+    return element.name + "  "+element. height
+})
+// Get array of all first names
+let fristName=characters.map(function(element, index){
+    return element.name.split(" ")[0]
+})
+
+
+//reducer 
+let name1=characters.reduce((acc,element)=> acc+element.mass,0)
+let name2=characters.reduce((acc,element)=> acc+element.name.length,0)
+//FILTER
+let heightFilter=characters.filter((element)=> element.mass >100)
+
+let genderFilter=characters.filter((element)=> element.gender==="female")
+
+//sort
+let sortByName=characters.sort((a,b)=>{
+  return a.name-b.mass
+  }
+  
+  );
+  // Another code
+  // function getMaxOfArray1(characters){
+  //   characters.sort().name;
+  //   console.log(characters);
+
+  // }
+  let sortByGender=characters.sort((a,b)=>{
+      return a.gender-b.height
+      }  
+      );
+    //EVERY
+    let every3=characters.every((element)=> element.eye_color==="blue")
+    let every4=characters.every((element)=> element.gender==="male")
+
+
+    // SOME
+
+
+  let somegender=characters.some((element)=> element.gender=== "male")
+  let somemass=characters.some((element)=> element.mass<50)
+  // end 
+//   Numbers' summations
+
+function sumAraary(a) {
+  let sum=0
+  for (let i=1; i<a.length;i++){ 
+     
+      sum +=a[i]
+  }
+  console.log(sum);
+}
+
+//  MaxOfArray
+function getMaxOfArray(numArray) {
+    return Math.max(null, numArray);
+  }
+
+  //reverse
+  function reverse(a) {
+    
+    for(let i=a.length-1;i>=0;i--)
+    console.log(a[i])
+}
+
+
+//statr
+  function start(a)
+  {
+    let string = "";
+    for (let i = 1; i <= a; i++) {
+      for (let j = 0; j < i; j++) {
+        string += "*";
+      }
+      string += "\n";
+    }
+    console.log(string)
+  
+}
+// 
+//while 
+function power(x){
+    let i = 2, count = false;
+        while (i <= x / 2) {
+            if (x % i == 0) {
+                count = true;
+                break;
+            }
+            i++;
+        }
+        if (!count) {
+            console.log(x + " is prime number")
+        } else {
+            console.log(x + " false otherwise.")
+        }
+}
+
+
+
